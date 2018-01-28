60dab8e971d40e49d03a74316ea8b9e30a4c3ba7
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2018-01-28 13:09:39 +0530

    Add files via upload
    changes to paypalbutton.js file

--- src/PaypalButton.js
+++ src/PaypalButton.js
@@ -9,6 +9,13 @@ const PayPalButton = paypal.Button.driver('react', {React, ReactDOM});
 
 export default class PayPalCheckout extends React.Component {
 
+    constructor(){
+        super();
+        this.state={
+            showResult:false
+        }
+    }
+
     onAuthorize(data, actions) {
         console.log('authorizing method is called here');
         // Optional: display a confirmation page here
@@ -28,15 +35,14 @@ export default class PayPalCheckout extends React.Component {
             document.querySelector('#zip').innerText       = shipping.postal_code;
             document.querySelector('#country').innerText   = shipping.country_code;
 
-           // document.querySelector('#paypal-button-container').style.display = 'none';
+            //document.querySelector('#paypal-button-container').style.display = 'none';
             document.querySelector('#confirm').style.display = 'block';
 
             // Listen for click on confirm button
 
-           document.querySelector('#confirmButton').addEventListener('onClick', (e) => {
+           document.querySelector('#confirmButton').addEventListener("click", (e) =>  {
                console.log('confirm button event class is called');
-                 // document.querySelector('#confirmButton').addEventListener('click', function() {
-
+                    
                 // Disable the button and show a loading message
 
                 document.querySelector('#confirm').innerText = 'Loading...';
@@ -46,14 +52,16 @@ export default class PayPalCheckout extends React.Component {
 
                 return actions.payment.execute().then(function() {
 
-                    // Show a thank-you note
+                // Show a thank-you note and clear the input box 
 
                     document.querySelector('#thanksname').innerText = shipping.recipient_name;
                     document.querySelector('#confirm').style.display = 'none';
                     document.querySelector('#thanks').style.display = 'block';  
+                    document.getElementById("amt_input_id").value='';
                 });
-            });
-    });
+            },false);
+        });
+    
             
     }
 
@@ -61,9 +69,9 @@ export default class PayPalCheckout extends React.Component {
     payment() {
 
         var inputamt=document.getElementById("amt_input_id").value;
-        if(inputamt.length<=0 || inputamt<=0){
-           alert('ttrueempty, zero or any string, Please enter a valid amount ');
-          //  <DialogBox/>
+        if( inputamt<=0){
+           alert('total amount cant be empty, zero or any string. Please enter a valid amount ');
+           // <DialogBox/>
             return true;
         }
         else{
@@ -77,9 +85,16 @@ export default class PayPalCheckout extends React.Component {
                         amount: {total: inputamt, currency: 'INR'}
                     }
                 ]
-            }); 
+            });
+            
+           //  this.setState({showResult:true}); 
+             
         }
     }
+
+   onErrror(err){
+       console.log('some error has occured, please try again properly')
+   }
  
                   /* add this code above amount line in transactins */
     /*  item_list: {
@@ -117,7 +132,6 @@ export default class PayPalCheckout extends React.Component {
 
     render() {
         const client = {
-              //sandbox:'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R'
            sandbox:'AUe1MOE728ao0-krDiGyQ9vrWwPhcPtRCKob0sljLA9d_UjKdqRh7ZRQAq6_LUX1XwKJr-JjkFKBStQ9' // my sandbox app client id
         };
 
@@ -131,103 +145,46 @@ export default class PayPalCheckout extends React.Component {
                             payment={this.payment}
                             commit={true} // Optional: show a 'Pay Now' button in the checkout flow
                             onAuthorize={this.onAuthorize}/>
-                <div >  {/* id="paypal-button-container" */} &nbsp; &nbsp; &nbsp; 
+                           
+                          
+                           { /*    { this.state.showResults ? <Results /> :null }  */} 
+                        
+                          
+                    <div /* id="paypal-button-container" */ >   &nbsp; &nbsp; &nbsp; 
+                        <div id="confirm" className="hidden" > 
+                            <div>Shipping to:</div>
+                            <div><span id="recipient"></span>, <span id="line1"></span>, <span id="city"></span></div>
+                            <div><span id="state"></span>, <span id="zip"></span>, <span id="country"></span></div>      
+                            &nbsp; &nbsp; &nbsp;  <button id="confirmButton">Complete Payment</button>
+                        </div>
+                        <div id="thanks" className="hidden">
+                            Thanks, <span id="thanksname"></span>!
+                        </div>
+                   </div>      
+            </div>
+        );
+    }
+}
+
+                    // trying to hide the shippind details and confirm payment button initially
+
+
+class Results extends React.Component{
+    render () {
+        return (
+            <div>
+                <div>  &nbsp; &nbsp; &nbsp; 
                     <div id="confirm" className="hidden" > 
                         <div>Shipping to:</div>
                         <div><span id="recipient"></span>, <span id="line1"></span>, <span id="city"></span></div>
                         <div><span id="state"></span>, <span id="zip"></span>, <span id="country"></span></div>      
                         &nbsp; &nbsp; &nbsp;  <button id="confirmButton">Complete Payment</button>
                     </div>
-                    <div id="thanks !" className="hidden">
-                         Thanks, <span id="thanksname"></span>!
+                    <div id="thanks" className="hidden">
+                       Thanks, <span id="thanksname"></span>!
                     </div>
                 </div>
-            </div>  
+            </div>
         );
     }
 }
-
-
-   {/* 
-        <div>
-        <div id="paypal-button-container"></div>
-        
-            <script>
-
-                paypal.Button.render({
-        
-                    env= 'sandbox', // sandbox | production
-        
-                    // PayPal Client IDs - replace with your own
-                    // Create a PayPal app: https://developer.paypal.com/developer/applications/create
-                    client= {
-                        sandbox:"AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R",
-                        production: '<insert production client id>'
-                    },
-        
-                    // Show the buyer a 'Pay Now' button in the checkout flow
-                    commit= 'true',
-        
-                    // payment() is called when the button is clicked
-                    payment= (data, actions) => {
-        
-                        // Make a call to the REST api to create the payment
-                        return actions.payment.create({
-                            payment: {
-                                transactions: [
-                                    {
-                                        amount: { total: '0.01', currency: 'USD' }
-                                    }
-                                ]
-                            }
-                        }); );
-                    },
-        
-                    // onAuthorize() is called when the buyer approves the payment
-                    onAuthorize: function(data, actions) {
-        
-                        // Make a call to the REST api to execute the payment
-                        return actions.payment.execute().then(function() {
-                            window.alert('Payment Complete!');
-                        });
-                    }
-        
-                }, '#paypal-button-container');
-        
-            </script>
-</div>
-
-
-
-
-
-
-
-
-
-        
-     let env='sandbox';
-        let client = {
-            sandbox:'A8vyPPy7mObxAPq5psJu6mY0fl34ASSoy89L7rBhFu6DB74pRimF3Jv1'
-         };
-
-         let payment = () => {
-            // ...
-            console.log('on authorize func called');
-        };
-    
-       let onAuthorize = (data, actions) => {
-          console.log('on authorize func called');
-        };
-		
-        let PayPalButton = paypal.Button.driver('react', { React, ReactDOM });
-
-        return (<div className='shoppingCart'>
-            <p>Buy <strong>Full Body Lobster Onesie - $24.99</strong> now!</p>
-
-            <PayPalButton
-                client={client}
-                payment={payment}
-                commit='true'
-                onAuthorize={onAuthorize} />
-        </div>);  */}
