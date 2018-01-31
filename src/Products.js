249890dafed4f73899154b9d51a18a6dd2653647
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2018-01-31 19:00:16 +0530

    updated product.js file now

--- src/Products.js
+++ src/Products.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn, } from 'material-ui/Table';
 import PropTypes from 'prop-types';
+import './App.css';
 
 
-const btn_style={             // to change the cursor pointer to hand shape when hover over button
+const btn_style={
     cursor:"pointer"
 }
+
 
 export default class Product extends React.Component{
     constructor(props){
         super(props);
-        this.state={ count:1, }             // initial state of the count 
+        this.state={ count:1,
+        }
     }
     
+
     addOne() {                                                       // adds one item when button clicked                                  
             this.setState(prevState => {
           return {count : prevState.count + 1 }
@@ -30,16 +34,32 @@ export default class Product extends React.Component{
          });
        }
 
-    calc(){                         // as we took count variable to display the qunatity, we multiplied price and count
-         var subtotal= Number(this.props.price * this.state.count).toFixed(2);
-         console.log(subtotal);
-         return subtotal;
+       calculate(){
+      //  var qty=document.getElementById('qty_div').value;
+        //var qty1=Number(qty);
+        // console.log(qty);
+        let qty = document.getElementById('qty_div').innerHTML;
+        let result = qty.match(/\d+/g);
+        console.log(result[0]);
+        let sub_price_value=document.getElementById("price_row").innerHTML;
+        let sub_price= sub_price_value.match(/\d+/g);
+        let sub_price_result=sub_price[0];
+       console.log(sub_price_result);
+    }
+
+    calc(){
+           // let qty = document.getElementById('qty_div').innerHTML;
+          //  let result = qty.match(/\d+/g);
+         // var subtotal= Number(this.props.price * result).toFixed(2);
+            var subtotal= Number(this.props.price * this.state.count).toFixed(2);
+            console.log(subtotal);
+            return subtotal;
           }
 
     render(){
         return(
             <div>
-                <Table >        
+                <Table >
                     <TableHeader displaySelectAll={false} adjustForCheckbox={false} >
                         <TableRow>
                             <TableHeaderColumn></TableHeaderColumn>
@@ -50,8 +70,10 @@ export default class Product extends React.Component{
                     </TableHeader>
                     <TableBody displayRowCheckbox={false} >
                         <TableRow >
-                            <TableRowColumn>{this.props.name}</TableRowColumn>
-                            <TableRowColumn>{this.props.description}</TableRowColumn>
+                            <TableRowColumn><img src={this.props.src}  width="70" height="70" 
+                                alt={this.props.alt} className="zoom" />
+                             </TableRowColumn>
+                            <TableRowColumn>{this.props.name}<br/> {this.props.description}</TableRowColumn>
                             <TableRowColumn id="price_row" >Price per each item:<br/> {this.props.price} <br/>
                             <button style={btn_style} onClick={this.calc.bind(this)}> Sub Total </button>
                           { /* <span id="show_subtotal"> {this.calc.bind(this)} </span> */}
