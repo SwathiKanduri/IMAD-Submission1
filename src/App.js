2e96cdd7fc3dc69c2a8f62afaeb50bffa5ebe807
SwathiKanduri <30653555+SwathiKanduri@users.noreply.github.com>
2018-02-11 10:09:29 +0530

    updated app.js to show only paypalbutton

--- src/App.js
+++ src/App.js
@@ -15,24 +15,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div>  
-          <Router> 
-              <div style={{float:'center'}} > 
-                <ul style={{listStyle:'none'}} >
-                  <li style={{display:'inline-block',paddingRight:20 }}>   
-                  <Link to="/" > 
-                  <IconButton iconClassName="material-icons" tooltip="Go Home"> home </IconButton> 
-                  </Link>  </li>
-                  <li style={{paddingLeft:20 ,display:'inline-block' }} >   
-                  <Link to="/FetchPaypal.js" >  go to paypal button </Link></li>
-                  <li style={{paddingLeft:20 ,display:'inline-block' }}>   
-                  <Link to="/Cart.js" > cart page  </Link>  </li>
-                </ul>
-                <Switch>
-                  <Route path="/FetchPaypal.js" component={FetchPaypal} /> 
-                  <Route path="/Cart.js" component={Cart} />   
-                </Switch>
-              </div>
-            </Router>
+          <FetchPaypal />
         </div>
       </div>
     );
